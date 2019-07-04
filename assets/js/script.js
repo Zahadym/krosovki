4c8f492c36f91da3ce794d77b6287833075de758
Zahadym <47822127+Zahadym@users.noreply.github.com>
2019-07-04 14:25:34 +0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-$(function() {
+$(function () {
+	$("#byrger").click(function (e) {
+			$(".login-byrger").toggleClass("byrger-menu");
+			$(".support-byrger").toggleClass("byrger-menu");
+			$(".byrger-filter").toggleClass("byrger-menu");
+			$(".header_nav-bg").toggleClass("header_nav-mb");
 
-	// Пользовательские функции
-
+	});
 });
