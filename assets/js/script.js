6a1b031739f8944aaccb66d2bfdfd5789c2e2918
Zahadym <47822127+Zahadym@users.noreply.github.com>
2019-07-05 10:29:40 +0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 $(function () {
 	$("#byrger").click(function (e) {
-			$(".login-byrger").toggleClass("byrger-menu");
-			$(".support-byrger").toggleClass("byrger-menu");
-			$(".byrger-filter").toggleClass("byrger-menu");
-			$(".header_nav-bg").toggleClass("header_nav-mb");
+		$(".login-byrger").toggleClass("byrger-menu");
+		$(".support-byrger").toggleClass("byrger-menu");
+		$(".byrger-filter").toggleClass("byrger-menu");
 
 	});
+	$("#open-support__akardeon").click(function (e) {
+		$(".target").toggleClass("akardion-support-none");
+		$("#target-1 a").toggleClass("support__link__akardion");
+	});
+	$("#open_filter").click(function (e) {
+		$(".point").toggleClass("akardion-support-none");
+		$("#target-2 a").toggleClass("support__link__akardion");
+	});
+
 });
